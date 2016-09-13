f5b6f40fd25ad0a174655b9320fecf0491f58e06
Christian Kleinboelting <kleinboelting@signal7.de>
2016-09-13 21:19:52 +0200

    Use AssertJ

--- src/test/java/foo/bar/FooPluginTest.java
+++ src/test/java/foo/bar/FooPluginTest.java
@@ -1,6 +1,5 @@
 package foo.bar;
 
-import foo.bar.FooTask;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.plugins.AppliedPlugin;
@@ -8,8 +7,7 @@ import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FooPluginTest {
 
@@ -25,14 +23,14 @@ public class FooPluginTest {
     public void foo_was_applied() {
         AppliedPlugin foo = project.getPluginManager().findPlugin("foo");
 
-        assertNotNull(foo);
+        assertThat(foo).isNotNull();
     }
 
     @Test
     public void foo_has_fooTask() {
         Task fooTask = project.getTasks().findByName("fooTask");
 
-        assertTrue(fooTask instanceof FooTask);
+        assertThat(fooTask instanceof FooTask).isTrue();
     }
 
 }
